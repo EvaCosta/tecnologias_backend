c9777823cd3140dc7d0de7156dbf529970b17fcb
Eva <evacosta.if@gmail.com>
2024-05-23 23:42:29 -0300

    ajuste server

--- webserver_basic_todo/server.js
+++ webserver_basic_todo/server.js
@@ -1,9 +1,8 @@
 const http = require('http');
-const { parse: parseUrl } = require('url');
+const url = require('url');
 const TaskController = require('./controllers/taskController');
 const TaskService = require('./services/taskService');
-const TaskRepository = require('./repositories/taskRepository');
-
+const TaskRepository = require('./repositories/taskRepository'); 
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -13,28 +12,31 @@ class Server {
     }
 
     handleRequest(req, res) {
-       
-        const { pathname } = parseUrl(req.url, true);
+        const parsedUrl = url.parse(req.url, true);
+        const { pathname } = parsedUrl;
         const method = req.method;
         
-        const routes = {
+        const routeHandlers = {
             'GET': {
-                '/tasks': this.controller.getTasks.bind(this.controller)
+                '/tasks': (controller, req, res) => controller.getTasks(req, res),
             },
             'POST': {
-                '/tasks': this.controller.addTask.bind(this.controller)
+                '/tasks': (controller, req, res) => controller.addTask(req, res),
             },
             'PUT': {
-                '/tasks/': this.controller.updateTask.bind(this.controller)
+                '/tasks/': (controller, req, res, id) => controller.updateTask(req, res, id),
             },
             'DELETE': {
-                '/tasks/': this.controller.deleteTask.bind(this.controller)
-            }
+                '/tasks/': (controller, req, res, id) => controller.deleteTask(req, res, id),
+            },
         };
-
-        const handler = routes[method] && routes[method][pathname];
-        if (handler) {
-            handler(req, res);
+        
+        const routeHandler = routeHandlers[method] && Object.entries(routeHandlers[method]).find(([path]) => pathname.startsWith(path));
+        
+        if (routeHandler) {
+            const [path, handler] = routeHandler;
+            const id = path === '/tasks/' ? pathname.split('/')[2] : null;
+            handler(this.controller, req, res, id);
         } else {
             res.writeHead(404, {'Content-Type': 'application/json'});
             res.end(JSON.stringify({ message: 'Rota não encontrada' }));
@@ -42,12 +44,10 @@ class Server {
     }
 
     start() {
-        const server = http.createServer((req, res) => this.handleRequest(req, res));
-        server.listen(port, hostname, () => {
+        http.createServer((req, res) => this.handleRequest(req, res)).listen(port, hostname, () => {
             console.log(`O servidor está sendo executado em http://${hostname}:${port}/`);
         });
     }
-
 }
 
 const taskRepository = new TaskRepository();
